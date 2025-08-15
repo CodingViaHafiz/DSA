4d857bc035778c48df81596900bb8edbfb4779f2
Hafiz Abdur rehman <hafiz1711205@gmail.com>
2025-08-15 10:26:44 +0500

    reverse_array

---
+++ day-6/reverse_array.js
@@ -0,0 +1,10 @@
+function reverse(arr) {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+}
+let arr = [1, 2, 3, 4, 5];
+reverse(arr);
+console.log(arr);
