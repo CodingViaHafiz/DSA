67ccef83676c36cbc8522cc684d4cb818e4d623a
adnanmirza1 <hafiz1711205@gmail.com>
2026-05-25 00:21:29 +0500

    practice

--- day-1/script.js
+++ day-1/script.js
@@ -1,36 +1,36 @@
 // Q1: Print all elements of an array
-// function printArr() {
-//   const arr = [1, 4, 2, 5];
-//   for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-//   }
-// }
-// printArr();
+function printArr() {
+  const arr = [1, 4, 2, 5];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+printArr();
 
 // Q2: Find the maximum element
-// function findMaxNum() {
-//   const arr = [34, 23, 83, 2, 10];
-//   let max = arr[0];
-//   for (const elem of arr) {
-//     if (elem > max) {
-//       max = elem;
-//     }
-//   }
-//   console.log("Maximum number is : ", max);
-// }
-// findMaxNum();
+function findMaxNum() {
+  const arr = [34, 23, 83, 2, 10];
+  let max = arr[0];
+  for (const elem of arr) {
+    if (elem > max) {
+      max = elem;
+    }
+  }
+  console.log("Maximum number is : ", max);
+}
+findMaxNum();
 
 //Q3: Reverse an array (create a new one)
-// function reverseArr() {
-//   const arr = [1, 2, 3, 4, 5, 6, 7];
-//   const reversed = [];
-//   for (let i = arr.length - 1; i >= 0; i--) {
-//     reversed.push(arr[i]);
-//   }
-//   console.log("original array: ", arr);
-//   console.log("reversed array: ", reversed);
-// }
-// reverseArr();
+function reverseArr() {
+  const arr = [1, 2, 3, 4, 5, 6, 7];
+  const reversed = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr[i]);
+  }
+  console.log("original array: ", arr);
+  console.log("reversed array: ", reversed);
+}
+reverseArr();
 
 // arr = [1, 2, 3, 4, 5, 6];
 // const sum = arr.reduce((acc, currVal) => {
--- day-8/filter-me.js
+++ day-8/filter-me.js
@@ -1,12 +1,12 @@
 function filterMe(arr) {
   let result = [];
   arr.map((num) => {
-    if (num > 5) {
+    if (num > 6) {
       result.push(num);
     }
   });
   return result;
 }
-let arr = [1, 73, 3, 4, 5, 6, 7, 78];
-console.log(`Before filtering: ${arr}`);
-console.log(`After filtering: ${filterMe(arr)}`);
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(`Before filtering ${arr}`);
+console.log(`After filtering ${filterMe(arr)}`);
