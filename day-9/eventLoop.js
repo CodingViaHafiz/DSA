5a17e73b7b1a112b092c3fc186f44035c3de50ee
adnanmirza1 <hafiz1711205@gmail.com>
2026-06-06 05:28:20 +0500

    sequential vs parallel fetching

--- day-9/eventLoop.js
+++ day-9/eventLoop.js
@@ -6,14 +6,41 @@
 // console.log("5");
 
 // Implement delay function that returns a Promise which resolves after a specified time
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+// function delay(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+// async function run() {
+//   console.log("start");
+//   await delay(1000);
+//   console.log("end");
+//   await delay(500);
+//   console.log("done");
+// }
+// run();
+
+// sequential vs parallel fetches
+const fakeApi = (id, ms) => {
+  return new Promise((res) =>
+    setTimeout(() => {
+      console.log(`Api ${id} finished`);
+      res(`Data for ${id} `);
+    }, ms),
+  );
+};
+async function sequential() {
+  const a = await fakeApi(1, 300);
+  const b = await fakeApi(2, 200);
+  const c = await fakeApi(3, 100);
+  return [a, b, c];
 }
-async function run() {
-  console.log("start");
-  await delay(1000);
-  console.log("end");
-  await delay(500);
-  console.log("done");
+sequential().then((result) => console.log("final result sequential:", result));
+
+async function parallel() {
+  const result = await Promise.all([
+    fakeApi(1, 300),
+    fakeApi(2, 200),
+    fakeApi(3, 100),
+  ]);
+  return result;
 }
-run();
+parallel().then((result) => console.log("final result parallel:", result));
